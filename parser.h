9511f07f6ba60dcf6cd755e6ce6a31e8fdb6ae3c
Aditya K <bejuzb0@gmail.com>
2020-01-31 22:47:34 +0530

    Token Write in a function

--- parser.h
+++ parser.h
@@ -6,6 +6,9 @@ char type[20][50] = {"KEYWORD", "IDENTIFIER", "RELATIONAL", "ASSIGNMENT", "ARITH
 
 
 int bracket = 0;
+void writefile(token *t,FILE *fw) {
+    fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', t->lexemename, ',', t->rw, ',', t->cl, ',', type[t->type-1], '>');
+}
 void parser(FILE* fr, FILE* fw) {
     for(;;) {
         token *s,*t,*u,*v,*w,*x, *y;
@@ -16,20 +19,20 @@ void parser(FILE* fr, FILE* fw) {
 			continue;
         else if(t->type == 12) {
             bracket++;
-            fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', t->lexemename, ',', t->rw, ',', t->cl, ',', type[t->type-1], '>');
+            writefile(t, fw);
         }
         else if(t->type == 13) {
             bracket--;
-            fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', t->lexemename, ',', t->rw, ',', t->cl, ',', type[t->type-1], '>');
+            writefile(t, fw);
         }
 		else if(t->type == 1) { // int
-            fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', t->lexemename, ',', t->rw, ',', t->cl, ',', type[t->type-1], '>');
+            writefile(t, fw);
             while((s = getNextToken(fr)) && s->type ==0);
-            fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', s->lexemename, ',', s->rw, ',', s->cl, ',', type[s->type-1], '>');
+            writefile(s, fw);
 
             if(s->type == 2) { //main
                 while((u = getNextToken(fr)) && u->type ==0);
-                fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', u->lexemename, ',', u->rw, ',', u->cl, ',', type[u->type-1], '>');
+                writefile(u, fw);
 
                 if(u->type == 10) { //(
                     //is a function
@@ -40,7 +43,7 @@ void parser(FILE* fr, FILE* fw) {
                     while((v = getNextToken(fr)) && v->type != 11) {
                         if(v->type == 0)
                             continue;
-                        fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', v->lexemename, ',', v->rw, ',', v->cl, ',', type[v->type-1], '>');
+                        writefile(v, fw);
                         while((w = getNextToken(fr)) && w->type ==0);
                         for(int i=0; i<5; i++) {
                             if(!strcmp(v->lexemename, datatype[i])) {
@@ -56,7 +59,7 @@ void parser(FILE* fr, FILE* fw) {
                         arg_count++;
                     }
                     
-                    fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', v->lexemename, ',', v->rw, ',', v->cl, ',', type[v->type-1], '>');
+                    writefile(v, fw);
                     s->typestr = "FUNC";
                     strcpy(s->arg_name,buff);
                     s->arg_no = arg_count;
@@ -71,7 +74,7 @@ void parser(FILE* fr, FILE* fw) {
                 }
                 else if(u->type == 15) {
                     while((x = getNextToken(fr)) && x->type == 0);
-                    fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', x->lexemename, ',', x->rw, ',', x->cl, ',', type[x->type-1], '>');
+                    writefile(x, fw);
                     if(x->type == 8) {
                        
                         int num = atoi(x->lexemename);
@@ -94,14 +97,14 @@ void parser(FILE* fr, FILE* fw) {
                     else if(x->type == 16) {
                         while((x = getNextToken(fr)) && x->type != 12) {
                             if(x->type != 0)
-                                fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', x->lexemename, ',', x->rw, ',', x->cl, ',', type[x->type-1], '>');
+                                writefile(x, fw);
                         }
-                        fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', x->lexemename, ',', x->rw, ',', x->cl, ',', type[x->type-1], '>');
+                        writefile(x, fw);
                         int num = 0;
                         
                         while(x->type != 13) {
                             while((x = getNextToken(fr)) && x->type == 0);
-                            fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', x->lexemename, ',', x->rw, ',', x->cl, ',', type[x->type-1], '>');
+                            writefile(x, fw);
                             //printf("%s %d\n", x->lexemename, x->type);
                             if(x->type == 5 || x->type == 8) {
                                 num++;
@@ -123,7 +126,7 @@ void parser(FILE* fr, FILE* fw) {
 
                     }
                     else {
-                        fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', x->lexemename, ',', x->rw, ',', x->cl, ',', type[x->type-1], '>');
+                        writefile(x, fw);
                     }
 
 
@@ -154,7 +157,7 @@ void parser(FILE* fr, FILE* fw) {
 
         }
         else {
-             fprintf(fw, "%c%s%c%d%c%d%c%s%c\n", '<', t->lexemename, ',', t->rw, ',', t->cl, ',', type[t->type-1], '>');
+             writefile(t, fw);
         }
 			
 	}
